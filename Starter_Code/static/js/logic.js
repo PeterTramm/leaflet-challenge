7a97a079ccde5379921a9ca040a66e6fecd43a89
peter.extra <peter.extra@outlook.com>
2023-12-22 13:23:35 +1100

    added some comments

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -1,25 +1,30 @@
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(d => {
     console.log(d)
-
+    
+    //Assigning d.features to data
     data = d.features
 
     console.log(data)
 
+    //Create inital map
     var map = L.map('map').setView([37.8, -96], 4);
 
+    //Create inital layout for map
     var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
+
+    //Defining base properties for circle markers
     var geojsonMarkerOptions = {
-        radius: 8,
         color: "#000",
         weight: 1,
         opacity: 1,
         fillOpacity: 0.8
     };
     
+    // Create circle markers based off features coordinates and properties
     L.geoJSON(data, {
         pointToLayer: function (feature, latlng) {
 
@@ -37,6 +42,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 
 });
 
+//Function to change circle colours depening on depth.
 function getColor(d) {
     var colorScale = d3.scaleLinear()
         .domain([0,10])
